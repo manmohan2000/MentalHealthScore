580a5bc275188fc2bd153c99d71bb8424460722f
Manmohan Joshi <manmohanjoshi2000@gmail.com>
2026-09-14 15:07:57 +0530

    updated js file with deployed backend link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:2200";
+  const API_BASE = "https://mentalhealthscore-0yh6.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
